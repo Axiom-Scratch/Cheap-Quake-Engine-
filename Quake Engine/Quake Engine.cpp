30a3fc7b9098ae90b8aba8e266a7c3c0451e9720
Dev <Dev@DESKTOP-KBQERQJ>
2026-03-03 22:38:15 -0800

    Add project files.

---
+++ Quake Engine/Quake Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Quake Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Quake Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
